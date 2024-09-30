94d6f5af7e2ba9cfbbf7af2d06e727209a565b46
Shawn <shawn.tang@beca.com>
2024-09-30 22:39:20 +1300

    exercise 3.14

--- phonebook/backend/index.js
+++ phonebook/backend/index.js
@@ -2,8 +2,14 @@ const express = require('express')
 var morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-// Middleware to parse JSON bodies
+
+require('dotenv').config()
+
 app.use(express.json())
+
+const Person = require('./models/person')
+
+const PORT = process.env.PORT
 
 // Middleware to capture JSON response
 morgan.token('body', function getBody (req) {
@@ -12,66 +18,26 @@ morgan.token('body', function getBody (req) {
 
 app.use(cors()) // Enable CORS
 app.use(express.static('dist'))
-// log response to console
 app.use(morgan(':method :status :url - :response-time ms :body '))
 
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "12345",
-      "id": "1"
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": "2"
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": "3"
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": "4"
-    },
-    {
-      "id": "5",
-      "name": "shawn tang",
-      "number": "3254234"
-    }
-]
-
-// Make a function to generate the id for the new person in the post request
-const generateId = () => {
-    const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id))): 0
-    return String(maxId + 1) // Return the maximum id + 1 as a string
-  }
-  
   // Make a post request to the /api/persons route
   app.post('/api/persons', (req, res) => {
-  
-    // Set the content of the person to the content of the request body
     const body = req.body
 
-    if (!body.content) {
+    if (body.name == undefined) {
       return res.status(400).json({
-        error: 'content missing'
+        error: 'name is missing'
       })
     }
-  
-    const person = {
-      content: body.content,
-      important: Boolean(body.important) || false, // If the important field is missing, the important field will be set to false
-      id: generateId()
-    }
-  
-    persons = persons.concat(person) // Add the new person to the person array
-  
-    res.json(person) // Send the new person as a response
-    console.log(person) // Log the new person to the console
+
+    const person = new Person({
+      name: body.name,
+      number: body.number
+    })
+
+    person.save().then(savedPerson => {
+      res.json(savedPerson)
+    })
   })
   
   // The code below defines two routes for the app. Thie first one defines an event handler to handle HTTP GET request made to the root.
@@ -83,32 +49,42 @@ const generateId = () => {
   })
   
   app.get('/api/persons', (req, res) => {
+    Person.find({}).then(persons => {
       res.json(persons)
+    })
   })
   
   // The colon syntax means that we will handle all requests that match the route /api/persons/:id
   app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const person = persons.find(person => person.id === id)
-  
-    if (person) {
+    Person.findById(req.params.id).then(person => {
       res.json(person)
-    } else {
-      res.status(404).end()
-    }
+    })
   })
   
   app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(person => person.id !== id)
-  
-    res.status(204).end()
+    Person.findByIdAndDelete(req.params.id).then(result => {
+      res.status(204).end()
+      console.log('Deleted')
+    })
+    .catch(error => next(error))
   })
-  
-  const PORT = process.env.PORT || 3000
   
   app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`)
   }) // This is a different listen from the one in http. This is an express method
 
+  app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+      name: body.name,
+      number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+      .then(updatedPerson => {
+        res.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
 
---
+++ phonebook/backend/models/person.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+mongoose.set('strictQuery', false) // By doing this, we can use the $push operator in the update method
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+// Because we know connect returns a promise, we can use async/await and then catch the response or the error
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+// Now we want to define the new note schema
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+// We want to remove the extra fields from the BSON object that is returned, and turn it into a JSON object
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+// Export the Note model
+module.exports = mongoose.model('Note', personSchema) // Export the Note model
