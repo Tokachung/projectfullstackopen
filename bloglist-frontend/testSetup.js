6bb39452dad29db3c66ae47ac74915230342986c
tokachung <st1403@live.com>
2025-04-01 20:48:20 +1100

    update vite config for testing setup

---
+++ bloglist-frontend/testSetup.js
@@ -0,0 +1,11 @@
+import { afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+
+// This file ensures that after each vitest test is run, the cleanup function executes to reset jsdom which simulates the browser
+// We need to update vite.config.js to use this
+
+afterEach(() => {
+    cleanup()
+})
+
