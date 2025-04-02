04012f729ada49df193ef5547f6b069435b0dd9f
tokachung <st1403@live.com>
2025-04-02 19:46:12 +1100

    added test for button click twice 5.15

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('renders blog', () => {
     const exampleBlog = {
@@ -20,14 +21,77 @@ test('renders blog', () => {
 
     render(<Blog blog={exampleBlog} likeBlog={likeBlog} removeBlog={removeBlog} />)
 
-    screen.debug()
-
-    const title = screen.getByText('React Hooks in Depth', { exact: false })
-    const author = screen.getByText('Dan Abramov', { exact: false })
-    const likes = screen.queryByText('42', { exact: false }) // Check if likes are rendered
+    const title = screen.getByText('React Hooks in Depth')
+    const author = screen.getByText('Dan Abramov')
+    const likes = screen.queryByText('42') // Check if likes are rendered
 
     expect(title).toBeDefined()
     expect(author).toBeDefined()
     expect(likes).toBeNull() // Ensure likes are NOT in the document initially
 
 })
+
+test('renders blog shows number of likes when button is clicked', async () => {
+    const exampleBlog = {
+        id: "1",
+        title: "React Hooks in Depth",
+        author: "Dan Abramov",
+        url: "https://react.dev/hooks",
+        likes: 42,
+        user: {
+          id: "123",
+          name: "John Doe",
+          username: "johndoe"
+        }
+    };
+
+    const likeBlog = vi.fn()
+    const removeBlog = vi.fn()
+
+    render(<Blog blog={exampleBlog} likeBlog={likeBlog} removeBlog={removeBlog} />)
+
+    const button = screen.getByText('view')
+
+    const user = userEvent.setup()
+    await user.click(button)
+
+    const likes = screen.queryByText('42', { exact: true }) // Check if likes are rendered
+    const url = screen.queryByText('https://react.dev/hooks')
+
+    expect(likes).toBeDefined() // Ensure likes are NOT in the document initially
+    expect(url).toBeDefined()
+})
+
+test('when button is clicked twice, event handler registers both clicks', async () => {
+    const exampleBlog = {
+        id: "1",
+        title: "React Hooks in Depth",
+        author: "Dan Abramov",
+        url: "https://react.dev/hooks",
+        likes: 42,
+        user: {
+          id: "123",
+          name: "John Doe",
+          username: "johndoe"
+        }
+    };
+
+    const likeBlog = vi.fn()
+    const removeBlog = vi.fn()
+
+    render(<Blog blog={exampleBlog} likeBlog={likeBlog} removeBlog={removeBlog} />)
+
+    const viewButton = screen.getByText('view')
+    const user = userEvent.setup()
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('Like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(likeBlog.mock.calls).toHaveLength(2)
+})
+
+
+
+
