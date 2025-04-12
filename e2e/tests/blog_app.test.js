26c0c803213b7c0a1916ad1cb7e473e8e915f72a
tokachung <st1403@live.com>
2025-04-12 19:04:50 +1000

    added login test

--- e2e/tests/blog_app.test.js
+++ e2e/tests/blog_app.test.js
@@ -1,7 +1,22 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3000/api/testing/reset')
+  
+    const response = await request.post('http://localhost:3000/api/users', {
+      data: {
+          name: "user",
+          username: "root",
+          password: "password"
+      }
+    });
+
+    console.log('Status:', response.status());
+    const body = await response.text();
+    console.log('Response body:', body);
+
     await page.goto('http://localhost:5173')
   })
 
@@ -10,13 +25,22 @@ describe('Blog app', () => {
     
     await page.getByRole('button', { name: 'login'}).click()
 
-    await expect(page.getByText('Please log in to view the blogs')).toBeVisible()
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
   })
+
+  test('Successful log in', async ({ page }) => {
+    await loginWith(page, 'root', 'password')
+    await expect(page.getByText('Wrong username or password')).not.toBeVisible()
+    await expect(page.getByText('user logged-in')).toBeVisible()
+    
+  })
+
+  test('Unsuccessful log in', async ({ page }) => {
+    await loginWith(page, 'root', 'wrongpassword')
+    await expect(page.getByText('Wrong username or password')).toBeVisible()
+    await expect(page.getByText('user logged-in')).not.toBeVisible()
+  })
 })
-
-
-
